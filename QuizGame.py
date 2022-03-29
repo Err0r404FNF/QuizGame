ef37902a5dc82edb48b806178a1ba2a06d4aac03
Err0r404FNF <102576101+Err0r404FNF@users.noreply.github.com>
2022-03-29 16:46:24 +1030

    Add files via upload

---
+++ QuizGame.py
@@ -0,0 +1,34 @@
+import time
+b=input("welcome to the sussy quiz, type start to start. (dont use capital letters!)")
+if b=="start":
+    a=input("do you like amogus?")
+if a=="yes":
+    print("cringe")
+if a=="no":
+    print("good")
+time.sleep(3)
+c=input("am i cool?")
+if c=="yes":
+    print("thanks :D")
+time.sleep(3)
+if c=="no":
+    print("you failed!")
+    time.sleep(3)
+    exit()
+d=input("Is your mum fat?")
+if d=="yes":
+    print("wtf bro")
+    time.sleep(2)
+    exit()
+if d=="no":
+    print("you're very nice, move on! :D")
+time.sleep(3)
+e=input("are you gay?")
+if e=="yes":
+    print("k")
+    time.sleep(1)
+if e=="no":
+    print("k")
+exitgame=input("congrats! you finished. there are no more questions yet but sub to Error404FileNotFound on youtube for updates! type exit to close")
+if exitgame=="exit":
+    exit()
